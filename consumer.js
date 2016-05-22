56a47f84076b7f998c335a5dceb4e2f5a21b47ba
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2016-05-22 22:14:50 +0200

    Updated consumer.js

--- consumer.js
+++ consumer.js
@@ -1,13 +1,7 @@
 const ZeroCSS = require('./lib');
 
-const zerocss = new ZeroCSS([
-  { name: 'xs', width: 0 },
-  { name: 'sm', width: 544 },
-  { name: 'md', width: 768 },
-  { name: 'lg', width: 992 },
-  { name: 'xl', width: 1200 },
-]);
+const zerocss = new ZeroCSS({ xs: 0, sm: 544, md: 768, lg: 992, xl: 1200 });
 
 zerocss.addSimpleUtil('myutil', 'coolshadow', 'box-shadow', '1px 1px rgba(0, 0, 0, 0.2)', true);
 
-zerocss.build(null, true);
+zerocss.build('mycss.css');
