0552516dac69ef03944c553a8fa9526efddc8ab3
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2016-07-05 18:41:42 -0700

    Tweaked code presentation in all-utils.js

--- src/all-utils.js
+++ src/all-utils.js
@@ -3,29 +3,23 @@ import ZeroCSS from './';
 const zerocss = new ZeroCSS({ xs: 0, sm: 544, md: 768, lg: 992, xl: 1200 });
 
 zerocss.addLoopUtils({ name: 'dis', property: 'display' }, {
-  0: 'none',
-  b: 'block',
-  i: 'inline',
-  ib: 'inline-block',
+  0: 'none', b: 'block', i: 'inline', ib: 'inline-block',
 });
 
 zerocss.addLoopUtils({ name: 'fl', property: 'float' }, {
-  0: 'none',
-  l: 'left',
-  r: 'right',
+  0: 'none', l: 'left', r: 'right',
 });
 
-zerocss.addLoopUtils({ name: 'h', property: 'height', pseudo: '' }, {
-  12: '12px',
-  14: '14px',
-  16: '16px',
-  '100%': '100%',
+zerocss.addLoopUtils({ name: 'h', property: 'height' }, {
+  12: '12px', 14: '14px', 16: '16px', '100%': '100%',
 });
 
 zerocss.addLoopUtils({ name: 'fs', property: 'font-size' }, {
-  16: '16px',
-  18: '18px',
-  24: '24px',
+  16: '16px', 18: '18px', 24: '24px',
+});
+
+zerocss.addLoopUtils({ name: 'deco', property: 'text-decoration', pseudo: 'hfa' }, {
+  0: 'none', u: 'underline',
 });
 
 const spacingValues = [1, 2, 3, 4, 5];
@@ -39,7 +33,7 @@ for (const s of spacingValues) {
       { short: 'r', long: 'right' },
     ]) {
       zerocss.addSimpleUtil(type.short,
-        `${direction.short}\,${s}`, `${type.long}-${direction.long}`, `${s}px`);
+        `${direction.short}\,${s}`, `${type.long}-${direction.long}`, `${s}px`, false);
     }
   }
 }
